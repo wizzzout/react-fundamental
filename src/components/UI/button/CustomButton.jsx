a82440998bfcec500eededbd89b9eefabd2a7bbe
wizzzoutMac <alexey@wizzzout.com>
2025-11-06 14:47:19 +0300

    Передал пропсы внутрь дочернего компонента элемента

--- src/components/UI/button/CustomButton.jsx
+++ src/components/UI/button/CustomButton.jsx
@@ -1,5 +1,9 @@
 import classes from './CustomButton.module.css'
 
-export default function CustomButton(props) {
-	return <button className={classes.CustomButton}>{props.children}</button>
+export default function CustomButton({ children, ...props }) {
+	return (
+		<button {...props} className={classes.CustomButton}>
+			{children}
+		</button>
+	)
 }
