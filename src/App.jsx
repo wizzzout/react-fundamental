76f780648cd983cab256280e0d607161be70ecb5
wizzzoutMac <alexey@wizzzout.com>
2025-11-06 13:00:44 +0300

    использова useState хук состояния

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css'
+import React, {useState} from "react";
 
 function App() {
-	return <></>
+    const [likes, setLikes] = useState(0)
+
+    function increment() {
+        setLikes(likes +1)
+    }
+    function decrement() {
+        setLikes(likes - 1)
+    }
+
+	return <>
+        <div className="App">
+            <h1>{likes}</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+        </div>
+    </>
 }
 
 export default App
