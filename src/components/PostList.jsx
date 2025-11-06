c4fad5bc358a6c63d0c3ca0d8f61ee2abda1500e
wizzzoutMac <alexey@wizzzout.com>
2025-11-06 14:29:38 +0300

    Реализовал компонент со списком постов и передачей данных через props

---
+++ src/components/PostList.jsx
@@ -0,0 +1,12 @@
+import PostItem from './PostItem'
+
+export default function PostList({ posts, title }) {
+	return (
+		<>
+			<h1 style={{ textAlign: 'center' }}>{title}</h1>
+			{posts.map(post => (
+				<PostItem post={post} key={post.id} />
+			))}
+		</>
+	)
+}
