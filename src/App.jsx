7a5152556e87c973105a0b9fb4b23ab1fdb540a2
wizzzoutMac <alexey@wizzzout.com>
2025-11-06 13:26:46 +0300

    Реализовал компонент и декомпозировал его в отдельный файл

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
 import './App.css'
-import React, {useState} from "react";
+import Counter from './components/Counter'
 
 function App() {
-    const [likes, setLikes] = useState(0)
-    const [value, setValue] = useState('Текст в инпуте')
-
-    function increment() {
-        setLikes(likes +1)
-    }
-    function decrement() {
-        setLikes(likes - 1)
-    }
-
-	return <>
-        <div className="App">
-            <h1>{likes}</h1>
-
-            <h2>{value}</h2>
-            <input
-                type="text"
-                value={value}
-                onChange={event => setValue(event.target.value)}
-            />
-
-            <button onClick={increment}>+</button>
-            <button onClick={decrement}>-</button>
-        </div>
-    </>
+	return (
+		<div className='App'>
+			<Counter />
+			<Counter />
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+export default function Counter() {
+	const [count, setCount] = useState(0)
+
+	function increment() {
+		setCount(count + 1)
+	}
+	function decrement() {
+		setCount(count - 1)
+	}
+
+	return (
+		<div>
+			<h1>{count}</h1>
+			<button onClick={increment}>+</button>
+			<button onClick={decrement}>-</button>
+		</div>
+	)
+}
