fdffd4af17dc145a7bca7e2261f3a68904579d1a
wizzzoutMac <alexey@wizzzout.com>
2025-11-06 14:14:23 +0300

    Реализовал список постов с помощью useState, компонента PostItem и метода массивов map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 import PostItem from './components/PostItem'
 
 function App() {
+	const [posts, setPost] = useState([
+		{ id: 1, title: 'React', content: 'Описание' },
+		{ id: 2, title: 'React 2', content: 'Описание 2' },
+		{ id: 3, title: 'React 3', content: 'Описание 3' },
+	])
+
 	return (
 		<div className='App'>
-			<PostItem post={{ id: 1, title: 'React', content: 'Описание' }} />
+			{posts.map(post => (
+				<PostItem post={post} />
+			))}
 		</div>
 	)
 }
