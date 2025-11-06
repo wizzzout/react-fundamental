b18ee4680d0d07c1964df096513298fa4230e1d8
wizzzoutMac <alexey@wizzzout.com>
2025-11-06 13:40:00 +0300

    Реализовал классовый компонент для ознакомления

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import ClassCounter from './components/ClassCounter'
 import Counter from './components/Counter'
 
 function App() {
 	return (
 		<div className='App'>
 			<Counter />
-			<Counter />
+			<ClassCounter />
 		</div>
 	)
 }
