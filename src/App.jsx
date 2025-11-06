8a600f94532b6b4b53216239a7672f20fd5d5f9f
wizzzoutMac <alexey@wizzzout.com>
2025-11-06 13:09:16 +0300

    Создал управляемый компонент при помощи двустороннего связывания.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 
 function App() {
     const [likes, setLikes] = useState(0)
+    const [value, setValue] = useState('Текст в инпуте')
 
     function increment() {
         setLikes(likes +1)
@@ -14,6 +15,14 @@ function App() {
 	return <>
         <div className="App">
             <h1>{likes}</h1>
+
+            <h2>{value}</h2>
+            <input
+                type="text"
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+
             <button onClick={increment}>+</button>
             <button onClick={decrement}>-</button>
         </div>
