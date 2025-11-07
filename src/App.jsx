2af6d9f7bf1c307c626804449f2706eb0c7341ad
wizzzoutMac <alexey@wizzzout.com>
2025-11-07 10:58:09 +0300

    Реализовал сортировку Компонент selecet, управляемые компонент, колбэк, сравнение с помощью localeCompare

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import PostAdd from './components/PostAdd'
 import PostList from './components/PostList'
+import CustomSelect from './components/UI/select/CustomSelect'
 
 function App() {
 	const [posts, setPosts] = useState([
-		{ id: 1, title: 'React', content: 'Описание' },
-		{ id: 2, title: 'React 2', content: 'Описание 2' },
-		{ id: 3, title: 'React 3', content: 'Описание 3' },
+		{ id: 1, title: 'BBB React', content: 'BBB Описание' },
+		{ id: 2, title: 'AAA React 2', content: 'CCC Описание 2' },
+		{ id: 3, title: 'CCC React 3', content: 'AAA Описание 3' },
 	])
+
+	const [selectedSort, setSelectedSort] = useState('')
+	const sortPosts = sort => {
+		setSelectedSort(sort)
+		setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+	}
 
 	const createPost = newPost => {
 		setPosts([...posts, newPost])
@@ -21,7 +28,19 @@ function App() {
 	return (
 		<div className='App'>
 			<PostAdd create={createPost} />
-			{posts.length !== 0 ? (
+			<hr style={{ margin: '15px 0' }} />
+			<div>
+				<CustomSelect
+					value={selectedSort}
+					onChange={sortPosts}
+					defaultValue='Сортировка'
+					options={[
+						{ value: 'title', name: 'По заголовку' },
+						{ value: 'content', name: 'По контенту' },
+					]}
+				/>
+			</div>
+			{posts.length ? (
 				<PostList
 					remove={removePost}
 					posts={posts}
---
+++ src/components/UI/select/CustomSelect.jsx
@@ -0,0 +1,19 @@
+export default function CustomSelect({
+	options,
+	defaultValue,
+	value,
+	onChange,
+}) {
+	return (
+		<select value={value} onChange={event => onChange(event.target.value)}>
+			<option value='' disabled>
+				{defaultValue}
+			</option>
+			{options.map(option => (
+				<option key={option.value} value={option.value}>
+					{option.name}
+				</option>
+			))}
+		</select>
+	)
+}
