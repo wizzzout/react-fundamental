8f0b06e448392ba47df8225be178853618b7d67e
wizzzoutMac <alexey@wizzzout.com>
2025-11-07 15:07:42 +0300

    Создал кастомный хук, декомпозировал App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useMemo, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import PostAdd from './components/PostAdd'
 import PostFilter from './components/PostFilter'
 import PostList from './components/PostList'
 import CustomButton from './components/UI/button/CustomButton'
 import Modal from './components/UI/modal/Modal'
+import { usePosts } from './hooks/usePosts'
 
 function App() {
 	const [posts, setPosts] = useState([
@@ -12,24 +13,9 @@ function App() {
 		{ id: 2, title: 'AAA React 2', content: 'CCC Описание 2' },
 		{ id: 3, title: 'CCC React 3', content: 'AAA Описание 3' },
 	])
-
 	const [filter, setFilter] = useState({ sort: '', query: '' })
 	const [modal, setModal] = useState(false)
-
-	const sortedPosts = useMemo(() => {
-		if (filter.sort) {
-			return [...posts].sort((a, b) =>
-				a[filter.sort].localeCompare(b[filter.sort])
-			)
-		}
-		return posts
-	}, [filter.sort, posts])
-
-	const sortedAndSearchedPosts = useMemo(() => {
-		return sortedPosts.filter(post =>
-			post.title.toLowerCase().includes(filter.query.toLowerCase())
-		)
-	}, [filter.query, sortedPosts])
+	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
 	const createPost = newPost => {
 		setPosts([...posts, newPost])
