be582a7fad71fa0864131ffbe552109da442dcf5
wizzzoutMac <alexey@wizzzout.com>
2025-11-07 14:41:58 +0300

    Добавил анимацию компонента (разобраться с реализацией)

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'motion/react'
 import PostItem from './PostItem'
 
 export default function PostList({ posts, title, remove }) {
@@ -8,14 +9,23 @@ export default function PostList({ posts, title, remove }) {
 	return (
 		<>
 			<h1 style={{ textAlign: 'center' }}>{title}</h1>
-			{posts.map((post, index) => (
-				<PostItem
-					remove={remove}
-					number={index + 1}
-					post={post}
-					key={post.id}
-				/>
-			))}
+			<AnimatePresence>
+				{posts.map((post, index) => (
+					<motion.div
+						key={post.id}
+						initial={{ opacity: 0, height: 0, x: 1 }}
+						animate={{ opacity: 1, height: 'auto' }}
+						exit={{ opacity: 0, height: 0 }}
+						transition={{ duration: 0.3 }}
+					>
+						<PostItem
+							remove={remove}
+							number={index + 1}
+							post={post}
+						/>
+					</motion.div>
+				))}
+			</AnimatePresence>
 		</>
 	)
 }
