c8b06cc34d6869f81d0be2e44d69e249ae2d0259
wizzzoutMac <alexey@wizzzout.com>
2025-11-16 18:02:19 +0300

    add comments post

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -17,7 +17,14 @@ export default class PostService {
 
 	static async getById(id) {
 		const response = await axios.get(
-			'https://jsonplaceholder.typicode.com/posts/' + id
+			`https://jsonplaceholder.typicode.com/posts/${id}`
+		)
+		return response
+	}
+
+	static async getCommentsById(id) {
+		const response = await axios.get(
+			`https://jsonplaceholder.typicode.com/posts/${id}/comments`
 		)
 		return response
 	}
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,26 +7,47 @@ import { useFetching } from '../hooks/useFetching'
 export default function PostIdPage() {
 	const params = useParams()
 	const [post, setPost] = useState({})
+	const [comments, setComments] = useState([])
 
-	const [fetchPostById, isLoading, error] = useFetching(async id => {
+	const [fetchPostById, postLoading, postError] = useFetching(async id => {
 		const response = await PostService.getById(id)
 		setPost(response.data)
 	})
 
+	const [fetchCommentsById, commentsLoading, commentsError] = useFetching(
+		async id => {
+			const response = await PostService.getCommentsById(id)
+			setComments(response.data)
+			console.log(response.data)
+		}
+	)
+
 	useEffect(() => {
 		fetchPostById(params.id)
+		fetchCommentsById(params.id)
 	}, [])
 
 	return (
 		<div>
 			<h1>Страница поста ID = {params.id}</h1>
 
-			{isLoading ? (
+			{postLoading ? (
 				<Loader />
 			) : (
 				<div>
 					{post.id} . {post.title}
 				</div>
+			)}
+
+			{commentsLoading ? (
+				<Loader />
+			) : (
+				comments.map(comment => (
+					<div key={comment.id} style={{ marginTop: '15px' }}>
+						<h5>{comment.email}</h5>
+						<div>{comment.body}</div>
+					</div>
+				))
 			)}
 		</div>
 	)
